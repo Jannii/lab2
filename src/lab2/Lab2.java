515be6cccf7c2425f3865e89a89d7ec7d935d9f2
Fredrik Nordström <fredrik.nordstrom@gmail.com>
2016-04-14 10:44:07 +0200

    adding a line

--- src/lab2/Lab2.java
+++ src/lab2/Lab2.java
@@ -16,6 +16,7 @@ public class Lab2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Testing to add a line of code");
         System.out.println("hej hej");
     }
     
